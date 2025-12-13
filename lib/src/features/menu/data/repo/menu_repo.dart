617e2f50e2f4f3633539d52cc68f17c72ad554ac
Muhamad Ishlah <nurul.ishlah@gmail.com>
2025-12-13 07:29:06 +0700

    Implement IsarDB singleton and IsarMenuRepo

--- lib/src/features/menu/data/repo/menu_repo.dart
+++ lib/src/features/menu/data/repo/menu_repo.dart
@@ -1,12 +1,14 @@
+import 'package:canteen_go/src/features/menu/data/local/menu_item_isar.dart';
 import 'package:canteen_go/src/features/menu/domain/models/menu_item.dart';
+import 'package:isar/isar.dart';
 
 abstract class MenuRepo {
-  Future<List<MenuItem>> fetchMenu();
+  Future<List<MenuItem>> fetchMenu({bool refresh = false});
 }
 
 class FakeMenuRepo implements MenuRepo {
   @override
-  Future<List<MenuItem>> fetchMenu() async {
+  Future<List<MenuItem>> fetchMenu({bool refresh = false}) async {
     await Future.delayed(const Duration(milliseconds: 350));
     return const [
       MenuItem(id: 'm1', name: 'Nasi Goreng', price: 20000),
@@ -17,5 +19,58 @@ class FakeMenuRepo implements MenuRepo {
       MenuItem(id: 'm6', name: 'Ayam Geprek', price: 23000),
       MenuItem(id: 'm7', name: 'Sate', price: 25000),
     ];
+  }
+}
+
+class IsarMenuRepo implements MenuRepo {
+  IsarMenuRepo(this._isar);
+
+  final Isar _isar;
+
+  @override
+  Future<List<MenuItem>> fetchMenu({bool refresh = false}) async {
+    final cached = await _isar.menuItemIsars.where().findAll();
+
+    // Ambil cached lokal
+    final cachedDomain = cached
+        .map(
+          (e) => MenuItem(
+            id: e.id,
+            name: e.name,
+            price: e.price,
+            available: e.available,
+            imageUrl: e.imageUrl,
+            category: e.category,
+          ),
+        )
+        .toList();
+
+    // Jika tidak refresh dan cached tidak kosong, kembalikan cached
+    if (!refresh && cachedDomain.isNotEmpty) return cachedDomain;
+
+    // Jika refresh, ambil remote
+    final remote = await FakeMenuRepo().fetchMenu();
+
+    // Simpan remote ke isar (Upsert)
+    await _isar.writeTxn(() async {
+      // mapping domain ke isar
+      final list = remote.map(MenuItemIsar.fromDomain).toList();
+      await _isar.menuItemIsars.putAll(list);
+    });
+
+    // kembalikan remote (data terbaru) dan perbaharui cached
+    final updated = await _isar.menuItemIsars.where().findAll();
+    return updated
+        .map(
+          (e) => MenuItem(
+            id: e.id,
+            name: e.name,
+            price: e.price,
+            available: e.available,
+            imageUrl: e.imageUrl,
+            category: e.category,
+          ),
+        )
+        .toList();
   }
 }
